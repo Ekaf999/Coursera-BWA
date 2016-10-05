bc3712dc52f58b44651cc5058fb4b6a0262c1e7e
Ekaf999 <fake999ekaf@gmail.com>
2016-10-06 01:35:03 +0200

    Delete data.items.component.js

--- Assignment4-solution/Assignment4-solutiongit/src/data.items.component.js
+++
@@ -1,8 +0,0 @@
-(function(){
-'use strict'
-
-angular.module('Data')
-.component('itemsComponent', {templateUrl:'views/list.template.html',
-                    bindings: {items: "<list"}});
-
-})();
